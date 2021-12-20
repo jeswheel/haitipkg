48ef1c7c25a1dbb117c3b19bbd4473b0ffc44d2e
Jesse Wheeler PC <jeswheel@umich.edu>
2021-12-20 12:00:17 -0500

    Created a new function to run the various vaccination campaigns for model3. The function is not finished and currently does not work

--- data-raw/MODEL3POMPDATA.R
+++ data-raw/MODEL3POMPDATA.R
@@ -5,7 +5,9 @@ library(tidyverse)
 MODEL3_INPUT_PARAMETERS <- yaml::read_yaml("https://raw.githubusercontent.com/ionides/haiti/main/model3/input/haiti-data/input_parameters.yaml?token=AQ356PKKXO27YIXW4X6WB3LBS2WS6")
 MODEL3_CASES <- readr::read_csv("https://raw.githubusercontent.com/ionides/haiti/main/model3/input/haiti-data/fromAzman/cases.csv?token=AQ356PJQTKMQMRK7GL5MKTTBS2WVK")
 MODEL3_RAIN <- readr::read_csv("https://raw.githubusercontent.com/ionides/haiti/main/model3/input/haiti-data/fromAzman/rainfall.csv?token=AQ356PPCCFFDQMHFHA7GQFTBS2WX4")
-MODEL3_PARAMS <- read.csv('https://raw.githubusercontent.com/ionides/haiti/main/model3/output/best_artibonite_params.csv?token=AQ356POMRAQP4HFCLVH7ZMDBS2W2I')
+MODEL3_PARAMS <- readr::read_csv('https://raw.githubusercontent.com/ionides/haiti/main/model3/output/best_artibonite_params.csv?token=AQ356POMRAQP4HFCLVH7ZMDBS2W2I')
+
+MODEL3_VACC_SCENARIOS <- readr::read_csv('https://raw.githubusercontent.com/ionides/haiti/main/model3/input/haiti-data/scenarios.csv?token=AQ356PO66YOHMAF6EBL3KFTBZHWZQ')
 
 MODEL3_CASES[MODEL3_CASES$date == '2016-10-01', 'Artibonite'] <- NA
 MODEL3_CASES[MODEL3_CASES$date == '2017-11-11', 'Artibonite'] <- NA
@@ -16,5 +18,6 @@ usethis::use_data(
   MODEL3_CASES,
   MODEL3_RAIN,
   MODEL3_PARAMS,
+  MODEL3_VACC_SCENARIOS
   overwrite = TRUE
 )
