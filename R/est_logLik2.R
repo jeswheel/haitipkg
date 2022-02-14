9e4444fdae57045888064f636e911eec790a3187
Josh Jiang <zhuoxunj@gmail.com>
2022-02-14 12:55:44 -0500

    added est_logLik2 function

---
+++ R/est_logLik2.R
@@ -0,0 +1,23 @@
+#' Estimate Log-Likelihood
+#'
+#' This function estimates the log-likelihood for model 2.
+#'
+#' @param model Input is a spatPomp object of the model whose Log-likelihood
+#' is to be estimated.
+#'
+#' @export
+est_logLik2 <- function(model) {
+
+  # Check that input is correct type
+  if (class(model) != "spatPomp"){
+    stop("Input is not a spatPomp model.")
+  }
+
+  # Get objective function for model
+  ofun <- traj_objfun(model)
+
+  # Get log-Likelihood
+  ll <- ofun(h2@params)
+
+  return(ll)
+}
